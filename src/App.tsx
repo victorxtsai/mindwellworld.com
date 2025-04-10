638a36cf358ce19ad06bb69490dd6dcfbee90e91
victorxtsai <71499306+victorxtsai@users.noreply.github.com>
2025-04-10 06:02:15 +0000

    valid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
 import Mission from './pages/Mission';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/mission" element={<Mission />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
